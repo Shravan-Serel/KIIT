5e2ba30f68480d0b7c6230a99f563bb19fa453c6
Shravan Serel <118686085+Shravan-Serel@users.noreply.github.com>
2024-03-24 14:06:19 +0530

    Create 8.cpp

---
+++ 8.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class SI
+{
+    private:
+        int p, n, r, si;
+    public:
+        void getDetails();
+        void calculate();
+};
+
+void SI::getDetails()
+{
+    cout << "Enter principal: ";
+    cin >> p;
+    cout << "Enter number of years: ";
+    cin >> n;
+    cout << "Enter rate of interest: ";
+    cin >> r;
+}
+
+void SI::calculate()
+{
+    si = p*(1+(r/100)*n);
+    cout << si;
+}
+
+int main()
+{
+    SI s;
+    s.getDetails();
+    printf("\nSimple Interest: ");
+    s.calculate();
+}
